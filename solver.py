6601a09115894ae674ef6a7c6c7a8a85c5f3efde
simon <>
2021-03-08 22:49:13 +0900

    Win_Loss computation : some simple stats to compare speed computation (number of calls, elapsed process time)

--- solver.py
+++ solver.py
@@ -1,14 +1,31 @@
 # Python solver for connect-four
 
+# for process_time()
+import time
+
 from board import Board
 
 class Win_Loss:
-    #def __init__(self):
-        # TODO: define here a table to store results
+    def __init__(self):
+        self.total_calls = 0
+        self.elapsed_time = 0
+
+    def compute_is_win(self, board):
+        self.total_calls = 0
+        t_start = time.process_time()
+        result = self.recursive_win_loss(board)
+        t_stop = time.process_time()
+        self.elapsed_time = t_stop - t_start
+        return result
+
+    def show_stats(self):
+        print("Elapsed time:", self.elapsed_time)
+        print("Total calls:", self.total_calls)
 
     # board is supposed to be a Board object
     # return True if the current player has a winning move
-    def compute_is_win(self, board, depth=0):
+    def recursive_win_loss(self, board, depth=0):
+        self.total_calls += 1
         for j in range(board.nb_cols):
             if depth<3:
                 s = ""
@@ -25,7 +42,7 @@ class Win_Loss:
                         board.remove_last_play()
                         return True
 
-                    result = self.compute_is_win(board, depth+1)
+                    result = self.recursive_win_loss(board, depth+1)
                     if result == False:
                         #opponent loses, so this is a win
                         board.remove_last_play()
@@ -37,5 +54,8 @@ class Win_Loss:
 if __name__ == "__main__":
     b = Board(nb_rows=4, nb_cols=5)
     b.show()
+    
     compute = Win_Loss()
-    print(compute.compute_is_win(b))
+    result = compute.compute_is_win(b)
+    print("is_win=", result)
+    compute.show_stats()
